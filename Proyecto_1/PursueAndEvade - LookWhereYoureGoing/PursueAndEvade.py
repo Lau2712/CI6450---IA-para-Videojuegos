b6dde3e2939c1d649ff219280ec9ffea229e76cf
Lau2712 <133783236+Lau2712@users.noreply.github.com>
2024-10-08 22:38:20 -0400

    Update PursueAndEvade.py

--- Proyecto_1/PursueAndEvade - LookWhereYoureGoing/PursueAndEvade.py
+++ Proyecto_1/PursueAndEvade - LookWhereYoureGoing/PursueAndEvade.py
@@ -10,34 +10,34 @@ class Pursue(DynamicSeek):
         self.evade = evade
     
     def getSteering(self) -> SteeringOutput:
-        # Calcular la dirección al objetivo
+        # Calculamos la dirección al objetivo
         direction = self.target.position - self.character.position
         distance = direction.magnitude()
 
-        # Calcular la velocidad actual
+        # Calculamos la velocidad actual
         speed = self.character.velocity.magnitude()
         
-        # Calcular el tiempo de predicción
+        # Calculamos el tiempo de predicción
         prediction = 0
         if speed <= distance / self.max_prediction:
             prediction = self.max_prediction
         else:
             prediction = distance / speed
             
-        # Calcular la posición futura del objetivo
+        # Calculamos la posición futura
         futurePosition = self.target.position + self.target.velocity * prediction
 
-        # Temporalmente cambiar la posición del objetivo a la posición futura
+        # Intercambiamos momentaneamente las posiciones
         actual_target_position = self.target.position
         self.target.position = futurePosition
         
-        # Obtener el steering utilizando el método de la clase padre (DynamicSeek)
+        # Usamos DynamicSeek para obtener el steering
         result = super().getSteering()
 
-        # Restaurar la posición original del objetivo
+        # Restauramos la posición actual
         self.target.position = actual_target_position
 
-        # Invertir la dirección si es evasión
+        # Si deseamos evadir, invertimos la velocidad
         if self.evade:
             result.linear = result.linear * -1
 
